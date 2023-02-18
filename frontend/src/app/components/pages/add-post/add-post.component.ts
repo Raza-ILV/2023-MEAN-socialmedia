ee7862c771624b94a3d0742b63b6dbbec514df9d
Raza <6125955@stud.nau.edu.ua>
2023-02-18 23:23:19 +0200

    Add-post page validation & styles

--- frontend/src/app/components/pages/add-post/add-post.component.ts
+++ frontend/src/app/components/pages/add-post/add-post.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-post.component.scss']
 })
 export class AddPostComponent {
-
+  post = {
+    title: "",
+    article: "",
+    postImage: "https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__340.jpg"
+  }
 }
--- frontend/src/app/services/validation.service.ts
+++ frontend/src/app/services/validation.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { News } from '../shared/models/News';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,13 @@ export class ValidationService {
 
   validateRegister(user:any){
     if(!user.username || !user.password){
+      return false
+    } else {
+      return true
+    }
+  }
+  validateAddPost(post:News){
+    if(!post.title || !post.article){
       return false
     } else {
       return true
