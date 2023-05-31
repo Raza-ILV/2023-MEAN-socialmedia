b3f72bc26f41d6eca381823a2bb9ae4677d43eaa
Raza-ILV <104830815+Raza-ILV@users.noreply.github.com>
2023-05-31 10:28:16 +0300

    Update database.js

--- backend/model/database.js
+++ backend/model/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 mongoose.set('strictQuery', true);
 module.exports ={
-    database: "mongodb+srv://master-user:035036503530@profiledb.dtepilu.mongodb.net/?retryWrites=true&w=majority",
+    database: "mongodb+srv://master-user:password0@profiledb.dtepilu.mongodb.net/?retryWrites=true&w=majority",
     secret: 'riven is op change my mind'
 }
